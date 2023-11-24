e34500ac09b281e103f8745611f511ec523219a8
Herb Miller <herb@bobbingwide.com>
2023-11-24 10:19:31 +0000

    Add PHPUnit tests for PHP 8.1 and PHP 8.2 #3

---
+++ tests/test-a-load-php.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @package oik-video
+ * @copyright (C) Copyright Bobbing Wide 2023
+ *
+ * Unit tests to load all the files for PHP 8.2, except batch ones
+ */
+
+class Tests_load_libs extends BW_UnitTestCase
+{
+
+	/**
+	 * set up logic
+	 *
+	 * - ensure any database updates are rolled back
+	 * - we need oik-googlemap to load the functions we're testing
+	 */
+	function setUp(): void
+	{
+		parent::setUp();
+
+	}
+
+	function test_load_admin() {
+		$this->load_dir_files( 'admin' );
+		$this->assertTrue( true );
+	}
+
+
+
+	function test_load_shortcodes() {
+		//$this->load_dir_files( 'shortcodes' );
+		oik_require( 'oik-video.inc', 'oik-video');
+		$this->assertTrue( true );
+	}
+
+
+
+	function load_dir_files( $dir, $excludes=[] ) {
+		$files = glob( "$dir/*.php");
+		//print_r( $files );
+
+		foreach ( $files as $file ) {
+			if ( !in_array( $file, $excludes ) ) {
+				//echo "Loading $file";
+				oik_require( $file, 'oik-video');
+			}
+		}
+	}
+
+	/**
+	 * Test that the plugin is loaded
+	 */
+	function test_load_plugin() {
+		oik_require( 'oik-video.php', 'oik-video');
+		$this->assertTrue( true );
+	}
+
+}
